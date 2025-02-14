6f38104a427624cddc05b58b4761472b3b512b17
Prabha <govindprabs@gmail.com>
2025-02-14 17:28:21 +0530

    Second Commit

--- src/test/java/Commands/Basics.java
+++ src/test/java/Commands/Basics.java
@@ -1,9 +1,28 @@
 package Commands;
 
+import java.awt.Dimension;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		 driver.get("https://omayo.blogspot.com/");
+		driver.manage().window().maximize();
+	//	driver.manage().window().fullscreen();
+		
+		//Dimension d = new Dimension(300,500);//not working
+	//	driver.manage().window().setSize(d);//
+		
+	driver.findElement(By.id("ta1")).sendKeys("hello");
+		//driver.findElement(By.id("alert1")).click();
+		driver.findElement(By.linkText("compendiumdev")).click();
+	
 		
 		
 
